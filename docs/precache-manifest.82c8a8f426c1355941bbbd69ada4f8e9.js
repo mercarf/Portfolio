90e6aae832c5a6696438ff8ed1948800f0b3723d
Mercedes <mcarballalfr@gmail.com>
2020-12-02 14:16:21 +0100

    add docs

---
+++ docs/precache-manifest.82c8a8f426c1355941bbbd69ada4f8e9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "825963a4bea14f706f0cdea1988c292d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e70bff49e73454144c2a",
+    "url": "./static/css/main.7f4b9946.chunk.css"
+  },
+  {
+    "revision": "4bdceb73f95ee0a9f23d",
+    "url": "./static/js/2.00df36e6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.00df36e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e70bff49e73454144c2a",
+    "url": "./static/js/main.427ac01d.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  },
+  {
+    "revision": "c94858c43c7d46f42b66d5ae8c4d480b",
+    "url": "./static/media/CV Mercedes Carballal.c94858c4.pdf"
+  },
+  {
+    "revision": "f761b5a453503eca2514f5bb082754b5",
+    "url": "./static/media/close bracket.f761b5a4.png"
+  },
+  {
+    "revision": "b8ba61fe1f9d0e5c8416ff2713170dad",
+    "url": "./static/media/github.b8ba61fe.svg"
+  },
+  {
+    "revision": "ba58718bddac3f274824060da3ded30c",
+    "url": "./static/media/pcweb.ba58718b.svg"
+  }
+]);
