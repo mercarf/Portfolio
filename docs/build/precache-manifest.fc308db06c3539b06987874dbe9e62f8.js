a29f6606c49eb160dd687d0be683831c75b6d339
Mercedes <mcarballalfr@gmail.com>
2020-12-04 10:41:51 +0100

    add docs

---
+++ docs/build/precache-manifest.fc308db06c3539b06987874dbe9e62f8.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb1a060789e54c99b2f31b287946bf9f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "161825f3d9a55e6f0608",
+    "url": "./static/css/main.6c568f3f.chunk.css"
+  },
+  {
+    "revision": "cb6e1766a88eee37beb4",
+    "url": "./static/js/2.47ebda29.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.47ebda29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "161825f3d9a55e6f0608",
+    "url": "./static/js/main.bd61f876.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  },
+  {
+    "revision": "c94858c43c7d46f42b66d5ae8c4d480b",
+    "url": "./static/media/CV Mercedes Carballal.c94858c4.pdf"
+  },
+  {
+    "revision": "4c12a6ddf7f8854f54250693d9bf2628",
+    "url": "./static/media/Mercedes.4c12a6dd.jpg"
+  },
+  {
+    "revision": "a99f9befb41454fd315b6067aacb9e9f",
+    "url": "./static/media/anonymous.a99f9bef.PNG"
+  },
+  {
+    "revision": "b8ba61fe1f9d0e5c8416ff2713170dad",
+    "url": "./static/media/github.b8ba61fe.svg"
+  },
+  {
+    "revision": "bb93beaf82a4b453eac052211997da02",
+    "url": "./static/media/gryffincode.bb93beaf.PNG"
+  },
+  {
+    "revision": "dde2a55cd3b9ec3b329f83e4b65513c7",
+    "url": "./static/media/js30.dde2a55c.PNG"
+  },
+  {
+    "revision": "93bf07d73dc9593b527dcab5c07e1759",
+    "url": "./static/media/numero.93bf07d7.PNG"
+  },
+  {
+    "revision": "ba58718bddac3f274824060da3ded30c",
+    "url": "./static/media/pcweb.ba58718b.svg"
+  },
+  {
+    "revision": "9b1547c70a0e10f13fce490d69b613b7",
+    "url": "./static/media/pokedex.9b1547c7.PNG"
+  },
+  {
+    "revision": "3aa73d6d54e7a5f023011624afde9d03",
+    "url": "./static/media/portfolio.3aa73d6d.PNG"
+  },
+  {
+    "revision": "9790dea66243a74a8e0733620d0e2d76",
+    "url": "./static/media/quinteto.9790dea6.PNG"
+  },
+  {
+    "revision": "1e8f137498f07164beda5fcd672334c1",
+    "url": "./static/media/rickymorty.1e8f1374.PNG"
+  },
+  {
+    "revision": "bf05a525a24a5535b8feb4808153361e",
+    "url": "./static/media/serie.bf05a525.PNG"
+  },
+  {
+    "revision": "d4a1aa43b068265b282daf23226e9d0b",
+    "url": "./static/media/smile.d4a1aa43.PNG"
+  },
+  {
+    "revision": "3a44eda8576590f02236522d3795dbbd",
+    "url": "./static/media/vsc.3a44eda8.png"
+  },
+  {
+    "revision": "dea1d6b0b7fe583af8e256f509fd3fc3",
+    "url": "./static/media/zeplin.dea1d6b0.svg"
+  },
+  {
+    "revision": "35d0a3eaae755cc55338a6480da62b6a",
+    "url": "./static/media/zodiaco.35d0a3ea.PNG"
+  }
+]);
